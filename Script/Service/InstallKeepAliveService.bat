e2fc9d08b9d621fe37f800b81d87f4f6839ff51d
John Charlton <john_charlton@sympatico.ca>
2016-09-27 12:44:46 -0400

    Keep Alive Service batch install files - fix ECHO wording

--- Script/Service/InstallKeepAliveService.bat
+++ Script/Service/InstallKeepAliveService.bat
@@ -2,7 +2,7 @@
 
 cd C:\Windows\Microsoft.NET\Framework\v4.0.30319
 
-echo Installing State Machine Service...
+echo Installing Keep IIS Alive Service...
 echo ---------------------------------------------------
 \debug
 installutil C:\Deployments\Services\KeepIISAliveService\1.0.0.0\Keebee.AAT.KeepIISAliveService.exe

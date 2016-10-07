b724328b41994c6050100b666e4274f6a9384e49
John <John@WIN10>
2016-10-07 14:58:33 -0400

    Add script to drop and recreate webuser login in database

---
+++ Script/Database/RemoveOldWebUser.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& 'C:\Deployments\Scripts\Database\PowerShell\DropOldWebUser.ps1'"
+PAUSE
--- Script/Database/SQL Server/DropOldWebuser.sql
+++ Script/Database/SQL Server/DropOldWebuser.sql
@@ -2,3 +2,9 @@ USE [KeebeeAAT]
 GO
 
 DROP USER [R2G2\webus]
+
+CREATE USER [R2G2\webuser] FOR LOGIN [R2G2\webuser] WITH DEFAULT_SCHEMA=[dbo]
+
+ALTER ROLE [db_datareader] ADD MEMBER [R2G2\webuser]
+
+ALTER ROLE [db_datawriter] ADD MEMBER [R2G2\webuser]

2ccc7a9f819e716f288da26b0df0e2a10fb5b499
John <John@WIN10>
2016-11-02 15:02:51 -0400

    Add a project for launching the Display App after a timer has expired

---
+++ UI/Keebee.AAT.DisplayLauncher/Launch.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Keebee.AAT.DisplayLauncher
+{
+    public partial class Launch : Form
+    {
+        private delegate void UpdateMessageDelegate();
+
+        private int _secondsRemaining;
+        private string _exePath;
+
+        public Launch()
+        {
+            InitializeComponent();
+
+            _exePath = ConfigurationManager.AppSettings["ExecutablePath"];
+            var waitTime = Convert.ToInt32(ConfigurationManager.AppSettings["WaitTime"]);
+            _secondsRemaining = waitTime;
+
+            var waitTimer = new Timer{ Interval = 1000 };
+            waitTimer.Tick += TimerTick;
+            waitTimer.Start();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            if (_secondsRemaining <= 0)
+            {
+                Process.Start(_exePath);
+                Close();
+            }
+            else
+            {
+                _secondsRemaining--;
+                UpdateMessage();
+            }
+        }
+
+        private void UpdateMessage()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new UpdateMessageDelegate(UpdateMessage));
+            }
+            else
+            {
+                lblMessage.Text = $"Display will launch in {_secondsRemaining} seconds...";
+            }
+        }
+    }
+}

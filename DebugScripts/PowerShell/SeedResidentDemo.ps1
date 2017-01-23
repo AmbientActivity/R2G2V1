16aafb3c30a99bdd85f791c4464507c25809090c
John <John@WIN10>
2017-01-23 11:34:08 -0500

    Put Debugging scripts into the DebugScripts folder

---
+++ DebugScripts/PowerShell/SeedResidentDemo.ps1
@@ -0,0 +1,28 @@
+﻿$server = $env:COMPUTERNAME + "\SQLEXPRESS"
+$database = "KeebeeAAT"
+$path = "C:\Deployments\Install\Database\SQL Server\"
+
+Try
+{
+    Write-Host -ForegroundColor yellow "`n--- Demo Resident ---`n`n"
+
+    # check if the database exists
+    $query = Invoke-SqlQuery -Query "SELECT COUNT(*) AS DatabaseCount FROM master.sys.databases WHERE name = N'$database'" -Server $server -Database "master"
+    $databaseCount = $query.DatabaseCount
+
+    # if the database doesn't exist, don't attempt anything
+    if ($databaseCount -eq 0) {
+        Write-Host -ForegroundColor yellow "`nR2G2 database does not exist.`n"
+    } 
+    else
+    {
+        Write-Host "Seeding demo resident...” -NoNewline
+        $queryFile = $path + "SeedResidentDemo.sql"
+        Invoke-SqlQuery -File $queryFile -Server $server -Database $database
+        Write-Host "done.`n”
+    }
+}
+Catch
+{
+    Write-Host -ForegroundColor red $_.Exception.Message
+}

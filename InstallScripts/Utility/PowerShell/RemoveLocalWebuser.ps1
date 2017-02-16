f4c1fbd713361ec2ffb6d19d3d61bd87efa679cb
John <John@WIN10>
2017-02-15 23:04:17 -0500

    RemoveLocalWebuser

--- InstallScripts/Utility/PowerShell/RemoveLocalWebuser.ps1
+++ InstallScripts/Utility/PowerShell/RemoveLocalWebuser.ps1
@@ -1,6 +1,6 @@
 ﻿Try
 {
-    Write-Host -ForegroundColor yellow "`n--- Local webuser ---`n"
+    Write-Host -ForegroundColor yellow "`n--- Reemove Local webuser ---`n"
 
     $computername = $env:COMPUTERNAME
     $ADSIComp = [adsi]"WinNT://$computername"

10a46715572ee2e72d6ee317d1ac286747c5a98d
John <John@WIN10>
2017-05-02 14:11:43 -0400

    File Cleanup scheduled task - don't delete Media Player library files

--- ScheduledTasks/Keebee.AAT.FileCleanup/Program.cs
+++ ScheduledTasks/Keebee.AAT.FileCleanup/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using Keebee.AAT.Shared;
 
@@ -10,11 +8,6 @@ namespace Keebee.AAT.FileCleanup
     {
         private static void Main()
         {
-            DeleteEmptyVideoCaptureFiles();
-            DeleteMediaPlayerLibraryFiles();
-        }
-        private static void DeleteEmptyVideoCaptureFiles()
-        {
             var root = new DirectoryInfo(VideoCaptures.Path);
             var folders = root.EnumerateDirectories().OrderBy(x => x.Name);
 
@@ -22,24 +15,6 @@ namespace Keebee.AAT.FileCleanup
                 .Where(f => f.Length == 0)).Select(x => $@"{x.Directory}\{x.Name}");
 
             foreach (var file in files)
-            {
-                try
-                {
-                    File.Delete(file);
-                }
-                catch
-                {
-                    // ignored
-                }
-            }
-        }
-        private static void DeleteMediaPlayerLibraryFiles()
-        {
-            var databasePath = $@"C:\Users\{Environment.UserName}\AppData\Local\Microsoft\Media Player";
-            var root = new DirectoryInfo(databasePath);
-            var dbFiles = root.EnumerateFiles().Select(x => $@"{x.Directory}\{x.Name}");
-
-            foreach (var file in dbFiles)
             {
                 try
                 {

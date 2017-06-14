3d96f1d7b25e9b85dadb158aa21f42902c626f5d
John <John@WIN10>
2017-06-14 14:03:59 -0400

    Service Activation/Deactivation - add Save button enable/disable behaviour

--- WebUI/Keebee.AAT.Administrator/Scripts/Services/Index.js
+++ WebUI/Keebee.AAT.Administrator/Scripts/Services/Index.js
@@ -11,13 +11,46 @@
         init: function (options) {
 
             var config = {
-                isInstalledBeaconWatcher: 0,
-                isInstalledVideoCapture: 0
+                IsInstalledBeaconWatcherService: 0,
+                IsInstalledVideoCaptureService: 0
             };
 
             $.extend(config, options);
 
             var cmdSave = $("#save");
+            var chkBeaconWatcher = $("#chkIsInstalledBeaconWatcher");
+            var chkVideoCapture = $("#chkIsInstalledVideoCapture");
+
+            var cmdSave = $("#save");
+
+            cmdSave.attr("disabled", "disabled");
+
+            function enableDetail() {
+                var isCheckedBeaconWatcher = $.trim(chkBeaconWatcher.is(":checked"));
+                var isCheckedVideoCapture = $.trim(chkVideoCapture.is(":checked"));
+
+                if ((isCheckedBeaconWatcher === "false" && config.IsInstalledBeaconWatcherService === 1)
+                    || (isCheckedBeaconWatcher === "true" && config.IsInstalledBeaconWatcherService === 0)) {
+                    cmdSave.removeAttr("disabled");
+                    return;
+                }
+
+                if ((isCheckedVideoCapture === "false" && config.IsInstalledVideoCaptureService === 1)
+                    || (isCheckedVideoCapture === "true" && config.IsInstalledVideoCaptureService === 0)) {
+                    cmdSave.removeAttr("disabled");
+                    return;
+                }
+
+                cmdSave.attr("disabled", "disabled");
+            }
+
+            chkBeaconWatcher.change(function() {
+                enableDetail();
+            });
+
+            chkVideoCapture.change(function () {
+                enableDetail();
+            });
 
             cmdSave.click(function () {
                 BootstrapDialog.show({
@@ -43,14 +76,16 @@
                                     waitMessage: "Saving settings...",
                                     verbage: "Settings saved successfully",
                                     params: {
-                                        activateBeaconWatcher: $.trim($("#chkIsInstalledBeaconWatcher").is(":checked")),
-                                        activateVideoCapture: $.trim($("#chkIsInstalledVideoCapture").is(":checked"))
+                                        activateBeaconWatcher: $.trim(chkBeaconWatcher.is(":checked")),
+                                        activateVideoCapture: $.trim(chkVideoCapture.is(":checked"))
                                     }
                                 }).then(function(data) {
                                     if (data.ErrorMessage === null) {
-                                        var json = data.ServiceSettings;
-                                        $(".chkIsInstalledBeaconWatcher").prop("checked", (json.IsInstalledBeaconWatcherService === 1));
-                                        $(".chkIsInstalledVideoCapture").prop("checked", (json.IsInstalledVideoCaptureService === 1));
+                                        $.extend(config, data.ServiceSettings);
+ 
+                                        chkBeaconWatcher.prop("checked", (config.IsInstalledBeaconWatcherService === 1));
+                                        chkVideoCapture.prop("checked", (config.IsInstalledVideoCaptureService === 1));
+                                        cmdSave.attr("disabled", "disabled");
                                     }
                                 });
                             }

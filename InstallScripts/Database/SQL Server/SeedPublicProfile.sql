625d7b7436f7177edadd14fcc378311cd1c7b7f9
WIN10\John <john_charlton@sympatico.ca>
2017-07-06 19:36:04 -0400

    Try once more to add Profile folder structure to git

--- InstallScripts/Database/SQL Server/SeedPublicProfile.sql
+++ InstallScripts/Database/SQL Server/SeedPublicProfile.sql
@@ -6,60 +6,72 @@ TRUNCATE TABLE PublicMediaFiles
 
 DECLARE @pathProfiles varchar(max)
 DECLARE @pathSharedLibrary varchar(max)
+DECLARE @mediaPathType varchar(max)
+
 SET @pathProfiles = FileTableRootPath() + '\Media\Profiles\'
 SET @pathSharedLibrary = FileTableRootPath() + '\Media\SharedLibrary\'
 
 --- Activity 1 - ResponseType "SlideShow" ---
+SELECT @mediaPathType = [Path] FROM MediaPathTypes WHERE Id = 3 -- images/general
 --- link from shared library
 INSERT INTO PublicMediaFiles (IsLinked, ResponseTypeId, MediaPathTypeId, StreamId)
-SELECT 1, 1, 3, StreamId FROM MediaFiles WHERE [Path] = @pathSharedLibrary + 'images\general\' AND [FileType] IN ('jpg', 'jpeg', 'png', 'bmp', 'gif')
-
+SELECT 1, 1, 3, StreamId FROM MediaFiles WHERE [Path] = @pathSharedLibrary + @mediaPathType + '\' AND [FileType] IN ('jpg', 'jpeg', 'png', 'gif')
 --- add from local profile
 INSERT INTO PublicMediaFiles (IsLinked, ResponseTypeId, MediaPathTypeId, StreamId)
-SELECT 0, 1, 3, StreamId FROM MediaFiles WHERE [Path] = @pathProfiles + '0\images\general\' AND [FileType] IN ('jpg', 'jpeg', 'png', 'bmp', 'gif')
+SELECT 0, 1, 3, StreamId FROM MediaFiles WHERE [Path] = @pathProfiles + '0\' +  @mediaPathType + '\' AND [FileType] IN ('jpg', 'jpeg', 'png', 'gif')
 
 --- Activity 2 - ResponseType "MatchingGame" ---
 -- link from shared library
+SELECT @mediaPathType = [Path] FROM MediaPathTypes WHERE Id = 7 -- activities\matching-game\shapes\
 INSERT INTO PublicMediaFiles (IsLinked, ResponseTypeId, MediaPathTypeId, StreamId)
-SELECT 1, 2, 7, StreamId FROM MediaFiles WHERE [Path] = @pathSharedLibrary + 'activities\matching-game\shapes\' AND [FileType] = 'png'
-INSERT INTO PublicMediaFiles (IsLinked, ResponseTypeId, MediaPathTypeId, StreamId)
-SELECT 1, 2, 8, StreamId FROM MediaFiles WHERE [Path] = @pathSharedLibrary + 'activities\matching-game\sounds\' AND [FileType] = 'mp3'
-
+SELECT 1, 2, 7, StreamId FROM MediaFiles WHERE [Path] = @pathSharedLibrary + @mediaPathType + '\' AND [FileType] = 'png'
 --- add from local profile
 INSERT INTO PublicMediaFiles (IsLinked, ResponseTypeId, MediaPathTypeId, StreamId)
-SELECT 0, 2, 7, StreamId FROM MediaFiles WHERE [Path] = @pathProfiles + '0\activities\matching-game\shapes\' AND [FileType] = 'png'
+SELECT 0, 2, 7, StreamId FROM MediaFiles WHERE [Path] = @pathProfiles + '0\' + @mediaPathType + '\' AND [FileType] = 'png'
+
+-- link from shared library
+SELECT @mediaPathType = [Path] FROM MediaPathTypes WHERE Id = 8 -- activities\matching-game\sounds\
 INSERT INTO PublicMediaFiles (IsLinked, ResponseTypeId, MediaPathTypeId, StreamId)
-SELECT 0, 2, 8, StreamId FROM MediaFiles WHERE [Path] = @pathProfiles + '0\activities\matching-game\sounds\' AND [FileType] = 'mp3'
+SELECT 1, 2, 8, StreamId FROM MediaFiles WHERE [Path] = @pathSharedLibrary + @mediaPathType + '\' AND [FileType] = 'mp3'
+--- add from local profile
+INSERT INTO PublicMediaFiles (IsLinked, ResponseTypeId, MediaPathTypeId, StreamId)
+SELECT 0, 2, 8, StreamId FROM MediaFiles WHERE [Path] = @pathProfiles + '0\' + @mediaPathType + '\' AND [FileType] = 'mp3'
 
 --- Activity 3 - ResponseType "Cats" ---
+SELECT @mediaPathType = [Path] FROM MediaPathTypes WHERE Id = 10 -- videos\cats\
 INSERT INTO PublicMediaFiles (IsLinked, ResponseTypeId, MediaPathTypeId, StreamId)
-SELECT 1, 3, 10, StreamId FROM MediaFiles WHERE [Path] = @pathSharedLibrary + 'videos\cats\' AND [FileType] = 'mp4'
+SELECT 1, 3, 10, StreamId FROM MediaFiles WHERE [Path] = @pathSharedLibrary + @mediaPathType + '\' AND [FileType] = 'mp4'
 
 --- Activity 5 - ResponseType "Radio" ---
 --- link from shared library
+SELECT @mediaPathType = [Path] FROM MediaPathTypes WHERE Id = 1 -- audio\music\
 INSERT INTO PublicMediaFiles (IsLinked, ResponseTypeId, MediaPathTypeId, StreamId)
-SELECT 1, 5, 1, StreamId FROM MediaFiles WHERE [Path] = @pathSharedLibrary + 'audio\music\' AND [FileType] = 'mp3'
-INSERT INTO PublicMediaFiles (IsLinked, ResponseTypeId, MediaPathTypeId, StreamId)
-SELECT 1, 5, 2, StreamId FROM MediaFiles WHERE [Path] = @pathSharedLibrary + 'audio\radio-shows\' AND [FileType] = 'mp3'
-
+SELECT 1, 5, 1, StreamId FROM MediaFiles WHERE [Path] = @pathSharedLibrary + @mediaPathType + '\' AND [FileType] = 'mp3'
 --- add from local profile
 INSERT INTO PublicMediaFiles (IsLinked, ResponseTypeId, MediaPathTypeId, StreamId)
-SELECT 0, 5, 1, StreamId FROM MediaFiles WHERE [Path] = @pathProfiles + '0\audio\music\' AND [FileType] = 'mp3'
+SELECT 0, 5, 1, StreamId FROM MediaFiles WHERE [Path] = @pathProfiles + '0\' + @mediaPathType + '\' AND [FileType] = 'mp3'
+
+--- link from shared library
+SELECT @mediaPathType = [Path] FROM MediaPathTypes WHERE Id = 2 -- audio\radio-shows\
 INSERT INTO PublicMediaFiles (IsLinked, ResponseTypeId, MediaPathTypeId, StreamId)
-SELECT 0, 5, 2, StreamId FROM MediaFiles WHERE [Path] = @pathProfiles + '0\audio\radio-shows\' AND [FileType] = 'mp3'
+SELECT 1, 5, 2, StreamId FROM MediaFiles WHERE [Path] = @pathSharedLibrary + @mediaPathType + '\' AND [FileType] = 'mp3'
+--- add from local profile
+INSERT INTO PublicMediaFiles (IsLinked, ResponseTypeId, MediaPathTypeId, StreamId)
+SELECT 0, 5, 2, StreamId FROM MediaFiles WHERE [Path] = @pathProfiles + '0\' + @mediaPathType + '\' AND [FileType] = 'mp3'
 
 --- Activity 6 - ResponseType "Television" ---
+SELECT @mediaPathType = [Path] FROM MediaPathTypes WHERE Id = 5 -- videos\tv-shows\
 --- link from shared library
 INSERT INTO PublicMediaFiles (IsLinked, ResponseTypeId, MediaPathTypeId, StreamId)
-SELECT 1, 6, 5, StreamId FROM MediaFiles WHERE [Path] = @pathSharedLibrary + 'videos\tv-shows\'
-
+SELECT 1, 6, 5, StreamId FROM MediaFiles WHERE [Path] = @pathSharedLibrary + @mediaPathType + '\' AND [FileType] = 'mp4'
 --- add from local profile
 INSERT INTO PublicMediaFiles (IsLinked, ResponseTypeId, MediaPathTypeId, StreamId)
-SELECT 0, 6, 5, StreamId FROM MediaFiles WHERE [Path] = @pathProfiles + '0\videos\tv-shows\'
+SELECT 0, 6, 5, StreamId FROM MediaFiles WHERE [Path] = @pathProfiles + '0\' + @mediaPathType + '\' AND [FileType] = 'mp4'
 
 --- Activity 7 - ResponseType "Ambient" ---
+SELECT @mediaPathType = [Path] FROM MediaPathTypes WHERE Id = 9 -- videos\ambient\
 INSERT INTO PublicMediaFiles (IsLinked, ResponseTypeId, MediaPathTypeId, StreamId)
-SELECT 1, 8, 9, StreamId FROM MediaFiles WHERE [Path] = @pathSharedLibrary + 'videos\ambient\' AND [FileType] = 'mp4'
+SELECT 1, 8, 9, StreamId FROM MediaFiles WHERE [Path] = @pathSharedLibrary + @mediaPathType + '\' AND [FileType] = 'mp4'
 
 -- VIEW THE RESULTS --
 

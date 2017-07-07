24e26e5d4545ea3d12e1a4f3700ad2a74d9551bd
WIN10\John <john_charlton@sympatico.ca>
2017-07-06 21:46:37 -0400

    CopyFlashBuilds.bat - change "John" to %USERNAME%

--- DebugScripts/CopyFlashBuildsLocal.bat
+++ DebugScripts/CopyFlashBuildsLocal.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& 'C:\Users\John\Source\Repos\R2G2V1\DebugScripts\PowerShell\CopyFlashBuildsLocal.ps1'"
+PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& 'C:\Users\%USERNAME%\Source\Repos\R2G2V1\DebugScripts\PowerShell\CopyFlashBuildsLocal.ps1'"
 echo.
 
 PAUSE

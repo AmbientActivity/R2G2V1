a543b60e1dd6b905bae236159b0377667058391c
WIN10\John <john_charlton@sympatico.ca>
2017-07-07 08:25:11 -0400

    Make a script for copying over needed 3rd party assemblies when building solution for the first time

---
+++ 3rdParty/Install3rdParty.ps1
@@ -0,0 +1,46 @@
+﻿Write-Host "Installing 3rd party assemblies...” -NoNewline
+
+# NReco Video Converter
+$pathNReco = "C:\Users\$env:USERNAME\Source\Repos\R2G2V1\3rdParty\NReco\"
+$pathThumbnailBin = "C:\Users\$env:USERNAME\Source\Repos\R2G2V1\Shared\Keebee.AAT.ThumbnailGeneration\bin\"
+$pathVideoBin = "C:\Users\$env:USERNAME\Source\Repos\R2G2V1\Shared\Keebee.AAT.VideoConversion\bin\"
+
+If (!(test-path "$pathThumbnailBin\Debug"))
+{
+    New-Item -ItemType Directory -Force -Path "$pathThumbnailBin\Debug" | Out-Null
+}
+Copy-Item "$pathNReco\NReco.VideoConverter.dll" "$pathThumbnailBin\Debug" -recurse -Force
+
+If (!(test-path "$pathThumbnailBin\Release"))
+{
+    New-Item -ItemType Directory -Force -Path "$pathThumbnailBin\Release" | Out-Null
+}
+Copy-Item "$pathNReco\NReco.VideoConverter.dll" "$pathThumbnailBin\Release" -recurse -Force
+
+
+If (!(test-path "$pathVideoBin\Debug"))
+{
+    New-Item -ItemType Directory -Force -Path "$pathVideoBin\Debug" | Out-Null
+}
+Copy-Item "$pathNReco\NReco.VideoConverter.dll" "$pathVideoBin\Debug" -recurse -Force
+Copy-Item "$pathNReco\NReco.VideoConverter.dll" "$pathVideoBin\Release" -recurse -Force
+
+# NReco Video Info
+If (!(test-path "$pathVideoBin\Release"))
+{
+    New-Item -ItemType Directory -Force -Path "$pathVideoBin\Debug" | Out-Null
+}
+Copy-Item "$pathNReco\NReco.VideoInfo.dll" "$pathVideoBin\Debug" -recurse -Force
+Copy-Item "$pathNReco\NReco.VideoInfo.dll" "$pathVideoBin\Release" -recurse -Force
+
+# Ajax Uploader license
+$pathNAjaxUploaderLicense = "C:\Users\$env:USERNAME\Source\Repos\R2G2V1\3rdParty\AjaxUploader\ajaxuploader4.lic"
+$pathAdministratorBin = "C:\Users\$env:USERNAME\Source\Repos\R2G2V1\WebUI\Keebee.AAT.Administrator\bin\"
+
+If (!(test-path $pathAdministratorBin))
+{
+    New-Item -ItemType Directory -Force -Path $pathAdministratorBin | Out-Null
+}
+Copy-Item $pathNAjaxUploaderLicense $pathAdministratorBin -recurse -Force
+
+Write-Host "done.”
--- DebugScripts/PowerShell/CopyFlashBuildsLocal.ps1
+++ DebugScripts/PowerShell/CopyFlashBuildsLocal.ps1
@@ -1,8 +1,9 @@
 ﻿Write-Host "Copying Flash builds...” -NoNewline
-$pathSwf = "C:\Users\$env:USERNAME\Source\Repos\R2G2V1\Flash\Builds\*"
-$pathDisplayDebug = "C:\Users\$env:USERNAME\Source\Repos\R2G2V1\UI\Keebee.AAT.Display\bin\Debug\"
-Copy-Item $pathSwf $pathDisplayDebug -recurse -Force
 
-$pathDisplayRelease = "C:\Users\$env:USERNAME\Source\Repos\R2G2V1\UI\Keebee.AAT.Display\bin\Release\"
-Copy-Item $pathSwf $pathDisplayRelease -recurse -Force
+$pathSwf = "C:\Users\$env:USERNAME\Source\Repos\R2G2V1\Flash\Builds\*"
+$pathDisplayBin = "C:\Users\$env:USERNAME\Source\Repos\R2G2V1\UI\Keebee.AAT.Display\bin"
+
+Copy-Item $pathSwf "$pathDisplayBin\Debug\" -recurse -Force
+Copy-Item $pathSwf "$pathDisplayBin\Release\" -recurse -Force
+
 Write-Host "done.”

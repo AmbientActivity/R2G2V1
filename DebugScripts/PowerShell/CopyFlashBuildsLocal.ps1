38a86ec5cc8fb9dc2183f81f7e5d10d4eba354cd
WIN10\John <john_charlton@sympatico.ca>
2017-07-06 21:41:23 -0400

    Allow bat files into the repository

---
+++ DebugScripts/PowerShell/CopyFlashBuildsLocal.ps1
@@ -0,0 +1,8 @@
+﻿Write-Host "Copying Flash builds...” -NoNewline
+$pathSwf = "C:\Users\$env:USERNAME\Source\Repos\R2G2V1\Flash\Builds\*"
+$pathDisplayDebug = "C:\Users\$env:USERNAME\Source\Repos\R2G2V1\UI\Keebee.AAT.Display\bin\Debug\"
+Copy-Item $pathSwf $pathDisplayDebug -recurse -Force
+
+$pathDisplayRelease = "C:\Users\$env:USERNAME\Source\Repos\R2G2V1\UI\Keebee.AAT.Display\bin\Release\"
+Copy-Item $pathSwf $pathDisplayRelease -recurse -Force
+Write-Host "done.”

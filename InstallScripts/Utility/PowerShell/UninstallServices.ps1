fcf5f7ca5f0b4f0d105eb3fb9e8c3bd61e69e889
WIN10\John <john_charlton@sympatico.ca>
2017-07-27 18:43:29 -0400

    After uninstalling a service also run Stop-Process to kill the task

--- InstallScripts/Utility/PowerShell/UninstallServices.ps1
+++ InstallScripts/Utility/PowerShell/UninstallServices.ps1
@@ -4,22 +4,27 @@
 
     Write-Host "Uninstalling Phidget Service..." -NoNewline
     Invoke-Command -ScriptBlock { C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /uninstall "C:\Deployments\Services\PhidgetService\1.0.0.0\Keebee.AAT.PhidgetService.exe"} | Out-Null
+    Stop-Process -ProcessName Keebee.AAT.PhidgetService* -Force
     Write-Host "done."
 
     Write-Host "Uninstalling Video Capture Service..." -NoNewline
     Invoke-Command -ScriptBlock { C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /uninstall "C:\Deployments\Services\VideoCaptureService\1.0.0.0\Keebee.AAT.VideoCaptureService.exe"} | Out-Null
+    Stop-Process -ProcessName Keebee.AAT.VideoCaptureService* -Force
     Write-Host "done."
 
     Write-Host "Uninstalling Bluetooth Beacon Watcher Service..." -NoNewline
     Invoke-Command -ScriptBlock { C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /uninstall "C:\Deployments\Services\BluetoothBeaconWatcherService\1.0.0.0\Keebee.AAT.BluetoothBeaconWatcherService.exe"} | Out-Null
+    Stop-Process -ProcessName Keebee.AAT.BluetoothBeaconWatcherService* -Force
     Write-Host "done."
 
     Write-Host "Uninstalling State Machine Service..." -NoNewline
     Invoke-Command -ScriptBlock { C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /uninstall "C:\Deployments\Services\StateMachineService\1.0.0.0\Keebee.AAT.StateMachineService.exe"} | Out-Null
+    Stop-Process -ProcessName Keebee.AAT.StateMachineService* -Force
     Write-Host "done."
 
     Write-Host "Uninstalling Keep IIS Alive Service..." -NoNewline
     Invoke-Command -ScriptBlock { C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /uninstall "C:\Deployments\Services\KeepIISAliveService\1.0.0.0\Keebee.AAT.KeepIISAliveService.exe"} | Out-Null
+    Stop-Process -ProcessName Keebee.AAT.KeepIISAliveService* -Force
     Write-Host "done."
 }
 Catch

ee8667825c932b7ef21899a4401e857cffd194ac
WIN10\John <john_charlton@sympatico.ca>
2017-07-29 18:27:10 -0400

    Put System.Process back in Simulator usings

--- UI/Keebee.AAT.Simulator/ControlPanel.cs
+++ UI/Keebee.AAT.Simulator/ControlPanel.cs
@@ -7,6 +7,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace Keebee.AAT.Simulator
